e35c3e382c8d4a7ec8b2496c6c16152ca33dbffd
FaithSK <524138064@qq.com>
2023-05-07 22:21:37 +0800

    update

--- src/api/main.js
+++
@@ -1,8 +0,0 @@
-
-
-// 导航列表
-import {list} from '@/api/content'
-export const listAPI = list
-
-import {navList} from '@/api/nav'
-export const navListAPI = navList
--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave:false,
-  // 配置端口号
-  devServer:{
-    port:3000
-  }
-})
